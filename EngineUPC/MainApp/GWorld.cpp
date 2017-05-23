e9c11730dd323bdcea664ba82cfe6e763214048c
Cristian Flores Shute <chris_fs313@hotmail.com>
2017-05-23 08:35:27 -0500

    Subiendo Debug/

--- EngineUPC/MainApp/GWorld.cpp
+++ EngineUPC/MainApp/GWorld.cpp
@@ -1,12 +1,20 @@
 #include "GWorld.h"
 
-
-
 GWorld::GWorld()
 {
 }
 
-
 GWorld::~GWorld()
 {
+}
+
+bool GWorld::ExistsTerrainIn(float x, float y) {
+	// TODO:
+
+
+	return false;
+}
+
+void GWorld::ExplodeTerrainIn(float x, float y, float radio) {
+	// TODO:
 }
--- EngineUPC/MainApp/GWorld.h
+++ EngineUPC/MainApp/GWorld.h
@@ -1,8 +1,20 @@
 #pragma once
+
+#include <FrameworkUPC\Sprite.h>
+
 class GWorld
 {
 public:
 	GWorld();
 	~GWorld();
+
+	bool ExistsTerrainIn(float x, float y);
+	void ExplodeTerrainIn(float x, float y, float radio);
+
+	void Update(float dt);
+	void Draw(float dt);
+
+private:
+	Sprite mSprite;
 };
 
